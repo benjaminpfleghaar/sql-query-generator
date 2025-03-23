290438e590cbc830194190aa556ee00c0dc44b24
benjaminpfleghaar <info@benjaminpfleghaar.com>
2025-03-23 16:54:14 +0100

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-// TODO create Readme
-
 import "./globals.css";
+import React from "react";
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Toaster } from "@/app/components/ui/sonner";
